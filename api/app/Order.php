dbc833921c4309606f69cd0d401990a13e5a73f2
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-03 02:42:25 +0500

    Update order.php, work updated.

--- api/app/Order.php
+++ api/app/Order.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // yahan pr saray table kay columns kay name detay hay phr validation is ky base pr hoty hay controller mayn..
+    protected $fillable = [
+        'id',
+        'order_title',
+        'product_id', // is order kay table mayn product id a rahy hay.. product ki id hogy 
+        'quantity',
+        'customer_email',
+        'customer_phone',
+    ];
+
+    public function product()
+    {
+        return $this->hasMany('App\Product');
+    }
 }
