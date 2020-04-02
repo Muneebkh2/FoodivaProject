ca76e19bfcf2b1e1c9e373020b2a0f19530f5c97
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-03 02:52:10 +0500

    Update api.php, api routes added.

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,17 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Authenticated routes...
+Route::group(['middleware' => 'auth:api'], function () {
+    // Route::get('users', 'Api\AuthController@getAuthUser');
 });
+
+// Auth Routes >>>
+Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
+Route::put('user/profile/edit/{id}', 'Api\UserController@updateUser');
+
+// Products Routes >>>
+Route::get('products/all', 'Api\ProductController@getAllProducts');
+Route::get('products/id/{id}', 'Api\ProductController@getProductsById');
+Route::post('products/create', 'Api\ProductController@createProduct');
+Route::put('producst/edit/{id}', 'Api\ProductController@updateProduct');
+Route::delete('products/delete/{id}', 'Api\ProductController@destroyProduct');
+
+// Order Routes >>>
+Route::get('orders/all', 'Api\OrderController@getAllOrders');
+Route::get('orders/id/{id}', 'Api\OrderController@getOrdersById');
+Route::post('orders/create', 'Api\OrderController@createOrder');
+Route::put('orders/edit/{id}', 'Api\OrderController@updateOrder');
+Route::delete('orders/delete/{id}', 'Api\OrderController@destroyOrder');
