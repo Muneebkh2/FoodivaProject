27a41b22863525275fb2c6a5469b71de23127a3b
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-03 02:42:39 +0500

    Update Product.php, work updated.

--- api/app/Product.php
+++ api/app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'id',
+        'name',
+        'sku',
+    ];
+
+    // haan abhy call nhn karwaya likh diya function dono main elk dosray ka..
+    // call ham controller mayn karwayengay jab zaroorat paray gy... 
+    // insert karty waqt or get kartay waqt ..
+    public function Category()
+    {
+        return $this->belongsToMany('App\Category');
+    }
+
+    public function Order()
+    {
+        return $this->hasOne('App\Order');
+    }
 }
