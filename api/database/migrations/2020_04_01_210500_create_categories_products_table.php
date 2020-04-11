eeed3900dce137b5db4b7520d5b82b8970d934f4
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-11 10:16:01 +0500

    categorey_product table name updated.

--- api/database/migrations/2020_04_01_210500_create_categories_products_table.php
+++ api/database/migrations/2020_04_01_210500_create_categories_products_table.php
@@ -13,7 +13,7 @@ class CreateCategoriesProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories_product', function (Blueprint $table) {
+        Schema::create('category_product', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('category_id');
             $table->unsignedBigInteger('product_id');
