2aa3f54fbfa6808e861c88255f0069aadb9a406c
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-12 20:58:03 +0500

    pages updated.

--- client/src/app/pages/index/index.component.ts
+++ client/src/app/pages/index/index.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.scss']
+    selector: 'app-index',
+    templateUrl: './index.component.html',
+    styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+    // brand slider Images
+    brandSlideImages = [
+        '../assets/images/brands/chinar.jpg',
+        '../assets/images/brands/mdh.jpg',
+        '../assets/images/brands/shan.png',
+        '../assets/images/brands/dabur.png',
+        '../assets/images/brands/haldirams.png',
+        '../assets/images/brands/amul.jpg',
+        '../assets/images/brands/ashoka.png'
+    ]
+    // set sliders options
+    BrandSlideOptions = {
+        margin: 10,
+        nav: true,
+        dots: false,
+        // navText : ["<i class='fas fa-arrow-left'></i>","<i class='fas fa-chevron-right'></i>"],
+        autoplay: true,
+        loop: true,
+        items: 5,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5
+            }
+        }
+    }
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void { }
+
 
 }
