d99e12e2f6540fee0b03918b2fc5a2ece6abfc65
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-16 21:04:42 +0500

    ProductComponent updated, products filter added.

--- client/src/app/pages/products/products.component.ts
+++ client/src/app/pages/products/products.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from 'src/app/services/rest-api.service';
 
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+    // data array's
+    allProducts = []
+    displayProducts = []
+    // var's 
+    searchValue: string = ''
+    searchText;
+    dataArray = []
 
-  ngOnInit(): void {
-  }
+    constructor(private api: RestApiService) {
+        this.getAllProducts()
+    }
 
+    ngOnInit(): void { }
+
+    getAllProducts() {
+        return this.api.getProducts().subscribe(
+            (res: any) => {
+                console.log(res)
+                this.allProducts = res
+                this.displayProducts = res
+            },
+            err => {
+                console.log(err)
+            }
+        )
+    }
+
+    searchProducts(value) {
+        console.log(value)
+        // let data = this.allProducts.filter(c => c.product.every(p => product.includes(c.id)))
+        // let data = this.allProducts[1]['product'].filter(element => element.id == 3)
+        // let data = this.allProducts.forEach((element) => {
+        //     console.log(element.product);
+        //     element.product
+        //     // this.dataArray.push(element.product)
+        // })
+        // console.log(data)
+        let filter = [1,3]
+        // console.log(this.allProducts.map(result => {
+        //     result.product = result.product.filter(course => (filter.includes(course.id)))
+        //     return result
+        // }))
+
+        // let courses = [1, 3];
+
+        const r = this.allProducts.filter(d => d.product.every(c => filter.includes(c.id)));
+        console.log(r);
+    }
 }
