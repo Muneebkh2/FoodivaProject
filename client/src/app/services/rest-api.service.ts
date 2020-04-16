5cf0eb67dad9ffbae1ebfc23948be46736acab06
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-16 21:07:54 +0500

    Update rest-api service.ts, products api end points added.

--- client/src/app/services/rest-api.service.ts
+++ client/src/app/services/rest-api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
 })
 export class RestApiService {
 
-    constructor() { }
+    host = 'http://127.0.0.1:8000/';
+    apiURL = this.host + 'api/';
+
+    constructor(private http: HttpClient) { }
+
+    // get all products
+    getProducts() {
+        return this.http.get(this.apiURL + 'getproducts');
+    }
+    // get products by id
+    getProductsByID(id) {
+        return this.http.get(this.apiURL + 'products/id/'+id);
+    }
+
 }
