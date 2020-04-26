63ad4d9a52899c9fc14faed6c3dd4d2989b3c827
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-26 23:53:57 +0500

    Update create-product_table, image column added.

--- api/database/migrations/2020_04_01_140210_create_product_table.php
+++ api/database/migrations/2020_04_01_140210_create_product_table.php
@@ -17,6 +17,7 @@ class CreateProductTable extends Migration
             $table->id();
             $table->string('name');
             $table->json('sku');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
