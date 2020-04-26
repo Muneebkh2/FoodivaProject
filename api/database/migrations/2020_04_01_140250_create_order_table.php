f2cf6095ffb73d7ebd2904f3afad9c3773e621ef
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-26 23:54:22 +0500

    Update create_order_table, order details added.

--- api/database/migrations/2020_04_01_140250_create_order_table.php
+++ api/database/migrations/2020_04_01_140250_create_order_table.php
@@ -15,6 +15,17 @@ class CreateOrderTable extends Migration
     {
         Schema::create('order', function (Blueprint $table) {
             $table->id();
+            $table->string('order_title');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('quantity');
+            $table->string('name');
+            $table->string('company_name');
+            $table->string('address');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('fax');
+            $table->string('payment_method');
             $table->timestamps();
         });
     }
