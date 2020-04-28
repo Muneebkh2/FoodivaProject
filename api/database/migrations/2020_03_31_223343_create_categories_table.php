eada22c53fe1ff404bee0bdbca1062aeec7d6470
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-28 23:07:00 +0500

    Update Category table migration, description column added.

--- api/database/migrations/2020_03_31_223343_create_categories_table.php
+++ api/database/migrations/2020_03_31_223343_create_categories_table.php
@@ -17,6 +17,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->longText('description')->nullable();
             $table->string('slug');
             $table->timestamps();
         });
