9cf1e7633189da09190fb73297f99a8bcd030338
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-04-28 23:07:29 +0500

    Add-category page completed.

--- client/src/app/pages/admin/categories/add-category/add-category.component.ts
+++ client/src/app/pages/admin/categories/add-category/add-category.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { RestApiService } from 'src/app/services/rest-api.service';
+import { NzNotificationService } from 'ng-zorro-antd';
 
 @Component({
-  selector: 'app-add-category',
-  templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.scss']
+    selector: 'app-add-category',
+    templateUrl: './add-category.component.html',
+    styleUrls: ['./add-category.component.scss']
 })
 export class AddCategoryComponent implements OnInit {
+    
+    submitted = false
+    // initalize form and form controls
+    categoryForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        description: new FormControl(''),
+    })
 
-  constructor() { }
+    // getter of formcontrols
+    get FC(){
+        return this.categoryForm.controls
+    }
 
-  ngOnInit(): void {
-  }
+    constructor(private server: RestApiService, private router: Router, private displayMsg: NzNotificationService) { }
+
+    ngOnInit(): void {}
+
+    addCategory(){
+        this.submitted = true
+        console.log(this.categoryForm.value)
+        let body = this.categoryForm.value
+
+        return this.server.createCategory(body).subscribe(
+            (res:any) => {
+                this.submitted = false
+                console.log("new category created.")
+                this.router.navigate(['/admin/category/all'])
+                this.displayMsg.create(
+                    'success', 'Success', res.message,
+                    {nzDuration: 4500}
+                );
+            },
+            (err:any) =>{
+                this.submitted = false
+                console.log("category creation failed: " + err)
+                this.displayMsg.create(
+                    'error', 'Error', err.error.message,
+                    {nzDuration: 3000}
+                );
+            }
+        )
+
+    }
 
 }
