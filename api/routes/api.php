7a554ed71b002cc32dff9e98bf4ae935be7163a8
Muneeb Ur Rehman <37962613+Muneebkh2@users.noreply.github.com>
2020-05-06 00:57:33 +0500

    API Project updated. (Laravel 5.8) added.

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,42 +3,43 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Auth Routes >>>
+Route::post('login', 'Api\UserController@login');
+Route::post('register', 'Api\UserController@register');
 
-// Authenticated routes...
-Route::group(['middleware' => 'auth:api'], function () {
-    // Route::get('users', 'Api\AuthController@getAuthUser');
-});
-
-// get-Products
-Route::get('getproducts', 'Api\CategoryController@getproducts');
 // ContactForm Email Route >>
 Route::post('sendWebQuery', 'Api\UserController@ContactForm');
 
 // Category Routes >>>
 Route::get('category/all', 'Api\CategoryController@getAllCategories');
 Route::get('category/slug/{slug}', 'Api\CategoryController@getCategoryByNAME');
-Route::get('category/id/{id}', 'Api\CategoryController@getCategoryById');
-Route::post('category/create', 'Api\CategoryController@createCategory');
-Route::put('category/edit/{id}', 'Api\CategoryController@updateCategory');
-Route::delete('category/delete/{id}', 'Api\CategoryController@destroyCategory');
 
 // Products Routes >>>
+Route::get('getproducts', 'Api\CategoryController@getproducts');// Products with category
 Route::get('products/all', 'Api\ProductController@getAllProducts');
 Route::get('products/id/{id}', 'Api\ProductController@getProductsById');
-Route::post('products/create', 'Api\ProductController@createProduct');
-Route::put('products/edit/{id}', 'Api\ProductController@updateProduct');
-Route::delete('products/delete/{id}', 'Api\ProductController@destroyProduct');
 
 // Order Routes >>>
 Route::get('orders/all', 'Api\OrderController@getAllOrders');
 Route::get('orders/id/{id}', 'Api\OrderController@getOrdersById');
-Route::post('orders/create', 'Api\OrderController@createOrder');
-Route::put('orders/edit/{id}', 'Api\OrderController@updateOrder');
-Route::delete('orders/delete/{id}', 'Api\OrderController@destroyOrder');
 
-// Auth Routes >>>
-Route::post('login', 'Api\UserController@login');
-Route::post('register', 'Api\UserController@register');
-Route::put('user/profile/edit/{id}', 'Api\UserController@updateUser');
+// Authenticated routes for admin panel...
+Route::group(['middleware' => 'auth:api'], function () {
+    // Category Routes >>>
+    Route::post('category/create', 'Api\CategoryController@createCategory');
+    Route::put('category/edit/{id}', 'Api\CategoryController@updateCategory');
+    Route::delete('category/delete/{id}', 'Api\CategoryController@destroyCategory');
+    
+    // Products Routes >>>
+    Route::post('products/create', 'Api\ProductController@createProduct');
+    Route::put('products/edit/{id}', 'Api\ProductController@updateProduct');
+    Route::delete('products/delete/{id}', 'Api\ProductController@destroyProduct');
+    
+    // Order Routes >>>
+    Route::post('orders/create', 'Api\OrderController@createOrder');
+    Route::put('orders/edit/{id}', 'Api\OrderController@updateOrder');
+    Route::delete('orders/delete/{id}', 'Api\OrderController@destroyOrder');
 
-
+    // Admin Profile Routes >>>
+    Route::put('user/profile/edit/{id}', 'Api\UserController@updateUser');
+});
